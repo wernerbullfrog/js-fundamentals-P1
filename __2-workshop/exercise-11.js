2e0957e489048acd068a28ad144cae14629d7263
Scott Morin <scottanthonymorin@gmail.com>
2021-02-03 15:54:53 -0500

    fix ex-11 explanation

--- __2-workshop/exercise-11.js
+++ __2-workshop/exercise-11.js
@@ -1,9 +1,9 @@
 function exercise11() {
   // Exercise 11
   //
-  // Write a program that creates a string that represents an 8×8 grid,
-  // using newline characters to separate lines. At each position of the grid
-  // there is either an "_" or a "#" character. The characters should form a chessboard.
+  // Write a program that creates a string that represents an 8×8 grid.
+  // At each position of the grid there is either an "_" or a "#" character.
+  // The characters should form a chessboard.
   // #_#_#_#_
   // _#_#_#_#
   // #_#_#_#_
