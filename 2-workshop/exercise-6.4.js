34e878074ff708b161e285f8e6d03ec7fe36d219
“Samuel <sam.laroche93@gmail.com>
2021-09-23 15:33:53 -0400

    Samuel Laroche-cd-wd-10/JSP1-EX-6_4

--- 2-workshop/exercise-6.4.js
+++ 2-workshop/exercise-6.4.js
@@ -6,6 +6,11 @@ function exercise6_4() {
   //
   // Write code between the lines (below)
   // -----------------------------------------------------------------
+  for (let i = 36; i >=0 ;i--) {
+    if(i%2 ===0 ){
+    console.log(i)
+    }
+  }
   // -----------------------------------------------------------------
   // Write code between the lines (above)
   //
@@ -15,7 +20,7 @@ function exercise6_4() {
   //
   //
 }
-
+exercise6_4()
 // Once you are done type the following in the terminal to test your answer(s).
 // yarn test exercise-6.4
 
